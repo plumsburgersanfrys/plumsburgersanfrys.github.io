f717eaf2c0a7b4cec0be39fb5d9f68b21d4c0aab
Reddstone35 <55559772+Reddstone35@users.noreply.github.com>
2021-12-30 12:01:26 -0700

    u
    this was stupid

---
+++ js/menu.js
@@ -0,0 +1,19 @@
+const burgers = [hambrbregur,booger,crappyBurger];
+const drinks = [pop,goodDewrinskwa,whiskey,crapyDrink];
+const meals = [totalyGudMeal,mealalsjminsanfasf,crappyMeal,yesMeal];
+const allItems = [meals,drinks,burgers];
+
+// burgers
+var hambrbregur;
+var booger;
+var crappyBurger;
+// drinks
+var pop;
+var goodDewrinskwa;
+var whiskey;
+var crapyDrink;
+// meals
+var totalyGudMeal;
+var mealalsjminsanfasf;
+var crappyMeal;
+var yesMeal;
--- js/order.js
+++ js/order.js
@@ -1,49 +1,12 @@
-const burgers = [hambrbregur,booger,crappyBurger];
-const drinks = [pop,goodDewrinskwa,whiskey,crapyDrink];
-const meals = [totalyGudMeal,mealalsjminsanfasf,crappyMeal,yesMeal];
-const allItems = [meals,drinks,burgers];
-
 var burgersAmount = 0;
 var mealsAmount = 0;
 var drinksAmount = 0;
-// burgers
-var hambrbregur;
-var booger;
-var crappyBurger;
-// drinks
-var pop;
-var goodDewrinskwa;
-var whiskey;
-var crapyDrink;
-// meals
-var totalyGudMeal;
-var mealalsjminsanfasf;
-var crappyMeal;
-var yesMeal;
+var specialsAmount = 0;
 
 var burgerPrice = 4.99;
 var drinkPrice = 2.50;
 var mealPrice = 9.99;
 var specialPrice = 7.50;
-
-for (entry in allItems) {
-  for (entry in burgers) {
-    if (entry === true) {
-      burgersAmount += 1
-    }
-  }
-  for (entry in drinks) {
-    if (entry === true) {
-      drinksAmount += 1
-    }
-  }
-  for (entry in meals) {
-    if (entry === true) {
-      mealsAmount += 1
-    }
-  }
-}
-
 
 var burgerPriceTotal = burgerPrice * burgersAmount;
 var drinkPriceTotal = drinkPrice * drinksAmount;
@@ -53,13 +16,57 @@ var priceTotal = burgerPriceTotal + mealPriceTotal + drinkPriceTotal;
 console.log(priceTotal);
 
 function orderButtonIsPressed(obj) {
-    if (obj.value == "Order") obj.value = "Ordered";
-    else obj.value = "Order";
+  if (obj.value == "Order"){
+    obj.value = "Ordered";
+    if (obj.classList[1] === "burgerorder") {
+      burgersAmount++;
+    }
+    if (obj.classList[1] === "drinkorder") {
+      drinksAmount++;
+    }
+    if (obj.classList[1] === "mealorder") {
+      mealsAmount++;
+    }
+    if (obj.classList[1] === "specialorder") {
+      specialsAmount++;
+    }
+  } else {
+    obj.value = "Order"
+    if (obj.classList[1] === "burgerorder") {
+      burgersAmount--;
+    }
+    if (obj.classList[1] === "drinkorder") {
+      drinksAmount--;
+    }
+    if (obj.classList[1] === "mealorder") {
+      mealsAmount--;
+    }
+    if (obj.classList[1] === "specialorder") {
+      specialsAmount--;
+    }
+  }
+  changeMoneyDue()
 }
+
 function checkoutOrder() {
   document.getElementById('opaque2').style.opacity = "1"
   document.getElementById('proccessing-box-order-maindiv').style.animation = "order-index-proccessing-box-anim ease-out 0.6s forwards";
   document.getElementById('order-progress-value').style.animation = "order-index-progress-fill-anim ease-out 2s infinite";
   var waitTime = Math.floor((Math.random() * 15000) + 6000);
   setTimeout(() => {  document.write(`<title>Error</title><p style="text-align: center;">An unknown error occurred whilst trying to complete purchase, try again later.</p>`); }, waitTime);
+}
+
+//var totalMoneyDue = document.getElementById('money-count')
+
+function changeMoneyDue() {
+  var burgerPriceTotal = burgerPrice * burgersAmount;
+  var drinkPriceTotal = drinkPrice * drinksAmount;
+  var mealPriceTotal = mealPrice * mealsAmount;
+  var specialPriceTotal = specialPrice * specialsAmount;
+
+  var priceTotal = burgerPriceTotal + mealPriceTotal + drinkPriceTotal + specialPriceTotal;
+
+  var priceTotal = priceTotal.toFixed(2)
+
+  document.getElementById("moneycount").innerHTML = priceTotal;
 }
